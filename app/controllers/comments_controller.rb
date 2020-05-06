286cbb8c607db75ffcb4c33324a512e5c1cb0ee1
Jun-Honda-0326 <60055417+Jun-Honda-0326@users.noreply.github.com>
2020-05-07 00:40:45 +0900

    initial_commit

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,14 @@
+class CommentsController < ApplicationController
+  def create
+    @comment = Comment.create(comment_params)
+    respond_to do |format|
+      
+      format.json
+    end
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:text).merge(user_id: current_user.id, tweet_id: params[:tweet_id])
+  end
+end
